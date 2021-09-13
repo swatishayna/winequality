ab56d17c8e1fe9a1e8a76ff7f78b231290eaa0f9
Swati <swati308mmu@gmail.com>
2021-09-13 08:47:33 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+1. create virtual environment
+2. activate the environment
+3. create requirements.txt
+4. install the requirements
---
+++ template.py
@@ -0,0 +1,26 @@
+import os
+
+
+
+dirs = [
+   os.path.join( "data","raw"),
+    os.path.join("data","processed"),
+    "notebooks",
+    "saved_models",
+    "src"
+]
+for dir in dirs:
+    os.mkdir(dir)
+    with open(os.path.join(dir, ".gitkeep"), "w") as f:
+        pass
+
+files = [
+    "dvc.yaml",
+    "params.yaml",
+    ".gitignore",
+    os.path.join("src", "__init__.py")
+    ]
+for file in files:
+    with open(file, "w") as f:
+        pass
+
